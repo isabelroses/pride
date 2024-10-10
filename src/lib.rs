1d0c7eb22f2a83d983996224976404353845abbe
comfysage <comfysage@isabelroses.com>
2024-10-10 23:09:16 +0200

    refactor: use `Color` struct and modify logic

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,23 @@ pub enum StyleType {
     Fg,
 }
 
-pub fn get_flag_color(flag: Flag) -> Vec<(u8, u8, u8)> {
+pub struct Color(u8, u8, u8);
+
+impl Color {
+    pub fn to_style(&self) -> Style {
+        Rgb(self.0, self.1, self.2)
+    }
+    pub fn is_dark(&self) -> bool {
+        let luminance = 0.2126 * f64::from(color.0)
+            + 0.7152 * f64::from(color.1)
+            + 0.0722 * f64::from(color.2);
+        luminance < 128.0
+    }
+}
+
+type Colors = Vec<(u8, u8, u8)>;
+
+pub fn get_flag_color(flag: Flag) -> Colors {
     match flag {
         Flag::Pride => vec![
             (255, 0, 0),
@@ -64,40 +80,38 @@ pub fn get_flag_color(flag: Flag) -> Vec<(u8, u8, u8)> {
 }
 
 pub fn apply_flag_color(text: &str, flag: Flag, style: StyleType, grouping: usize) -> String {
-    let colors = get_flag_color(flag);
+    let colors: Colors = get_flag_color(flag);
     let mut styled_text = String::new();
 
-    let fin_grouping = if grouping == 0 && text.len() >= colors.len() {
-        text.len() / colors.len()
-    } else if grouping == 0 {
-        1
+    let fin_grouping = if grouping == 0 {
+        if text.len() >= colors.len() {
+            text.len() / colors.len()
+        } else {
+            1
+        }
     } else {
         grouping
     };
 
     for (i, c) in text.chars().enumerate() {
-        let color = &colors[(i / fin_grouping) % colors.len()];
+        let color: Color = &colors[(i / fin_grouping) % colors.len()];
         let styled_char = match style {
             StyleType::Bg => {
-                let luminance = 0.2126 * f64::from(color.0)
-                    + 0.7152 * f64::from(color.1)
-                    + 0.0722 * f64::from(color.2);
-
                 // Choose black or white for fg based on luminance
-                let fg_color = if luminance > 128.0 {
-                    Rgb(0, 0, 0) // Dark text for light background
-                } else {
+                let fg_color = if color.is_dark() {
                     Rgb(255, 255, 255) // Light text for dark background
+                } else {
+                    Rgb(0, 0, 0) // Dark text for light background
                 };
 
                 &Style::new()
-                    .on(Rgb(color.0, color.1, color.2))
+                    .on(color.to_style())
                     .fg(fg_color)
                     .paint(c.to_string())
                     .to_string()
             }
             StyleType::Fg => &Style::new()
-                .fg(Rgb(color.0, color.1, color.2))
+                .fg(color.to_style())
                 .paint(c.to_string())
                 .to_string(),
         };
